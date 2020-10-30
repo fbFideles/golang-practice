9535a33fab8f630b6bdcbde19e238bf5990eb09d
marcelobezer <marcelobez.g@gmail.com>
2020-10-30 09:36:39 -0300

    fixed sub bug where first value is also a negative one; now the command do: x -y -z, NOT -x -y -z

--- gauss/operations.go
+++ gauss/operations.go
@@ -61,6 +61,7 @@ func (o *Operation) Sub(flag bool) (sub interface{}, err error) {
 	var (
 		intSub   int64
 		floatSub float64
+		firt     bool = true
 	)
 
 	if len(o.Values) <= 1 {
@@ -80,7 +81,14 @@ func (o *Operation) Sub(flag bool) (sub interface{}, err error) {
 				log.Fatal(err)
 			}
 
-			intSub -= number
+			if firt {
+				intSub = number
+			} else {
+				intSub -= number
+			}
+
+			firt = false
+
 		} else {
 			s := num.(string)
 			number, err := strconv.ParseFloat(s, 64)
@@ -88,7 +96,13 @@ func (o *Operation) Sub(flag bool) (sub interface{}, err error) {
 				log.Fatal(err)
 			}
 
-			floatSub -= number
+			if firt {
+				floatSub = number
+			} else {
+				floatSub -= number
+			}
+
+			firt = false
 		}
 	}
 
