7bd53b63de7974ca528d3ece4670e778090aef43
Felipe Fideles <felipebreno106@gmail.com>
2020-10-30 08:19:08 -0300

    adding product operation (#10)

--- gauss/operations.go
+++ gauss/operations.go
@@ -80,3 +80,33 @@ func (o *Operation) Sub(flag bool) (sub interface{}, err error) {
 
 	return
 }
+
+// Prod -
+func (o *Operation) Prod(flag bool) (prod interface{}, err error) {
+	if len(o.Values) <= 1 {
+		return nil, errors.New("It's not possible to give the product of less than 2 operands")
+	}
+
+	if flag {
+		prodTemp := int64(0)
+		for _, num := range o.Values {
+			var s string = num.(string)
+			number, err := strconv.ParseInt(s, 10, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			prodTemp *= number
+		}
+
+		prod = prodTemp
+	} else {
+		prodTemp := float64(0)
+		for _, num := range o.Values {
+			prodTemp *= num.(float64)
+		}
+
+		prod = prodTemp
+	}
+
+	return
+}
